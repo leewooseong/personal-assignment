8baec45d65daae757df5852cf3463e98ab776c49
leewooseong <sitpo96@naver.com>
2022-06-17 01:20:28 +0900

    feat :  네비게이션 하이라이팅 추가

--- src/routes/shared/GNB/index.tsx
+++ src/routes/shared/GNB/index.tsx
@@ -1,28 +1,45 @@
-import styles from './GNB.module.scss'
-import { GoalIcon, TrainingIcon, HistoryIcon, HomeIcon } from 'assets/svgs'
-import { Link } from 'react-router-dom'
+import { Link, Location, useLocation } from 'react-router-dom'
+import cx from 'classnames'
 
-const MENULIST = [
+import { GoalIcon, TrainingIcon, HistoryIcon, HomeIcon } from 'assets/svgs'
+import styles from './GNB.module.scss'
+
+const GNB_LIST = [
+  { name: 'Home', icon: <HomeIcon />, pageLink: '/' },
   { name: 'Goals', icon: <GoalIcon />, pageLink: '/detail/goals' },
   { name: 'Trainings', icon: <TrainingIcon />, pageLink: '/detail/trainings' },
   { name: 'History', icon: <HistoryIcon />, pageLink: '/detail/history' },
 ]
 
-const GNBLIST = [{ name: 'Home', icon: <HomeIcon />, pageLink: '/' }, ...MENULIST]
+const findPageName = (location: Location, ButtonName: string) => {
+  const locationPageName = location.pathname.split('/').pop()
 
-const GNB = () => (
-  <nav className={styles.gnbWrapper}>
-    <ul className={styles.gnbList}>
-      {GNBLIST.map((menu, index) => (
-        <Link to={menu.pageLink} key={`GNB-${index + 1}`}>
-          <li className={styles.gnbItem}>
-            {menu.icon}
-            {menu.name}
-          </li>
-        </Link>
-      ))}
-    </ul>
-  </nav>
-)
+  return locationPageName === ButtonName.toLowerCase()
+}
+
+const GNB = () => {
+  const location = useLocation()
+  const menuNameExample = 'hihi'
+
+  return (
+    <nav className={styles.gnbWrapper}>
+      <ul className={styles.gnbList}>
+        {GNB_LIST.map((menu, index) => (
+          <Link to={menu.pageLink} key={`GNB-${index + 1}`}>
+            <li
+              className={cx(styles.gnbItem, {
+                [styles[menu.name.toLowerCase()]]: findPageName(location, menu.name),
+                [styles.active]: findPageName(location, menu.name),
+              })}
+            >
+              {menu.icon}
+              {menu.name}
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  )
+}
 
 export default GNB
